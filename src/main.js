62336fac85b472424629a2c1d16ba95345a35d5d
Shujaat Azim <shujaat.azim@gmail.com>
2021-10-24 23:05:37 -0700

    Added comments.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import k from './kaboom'
 import Title from './scenes/Title'
 import Game from './scenes/Game'
 
+// all scenes are to be "resgistered" here
+
 k.scene("title", Title)
 k.scene("game", Game)
 
